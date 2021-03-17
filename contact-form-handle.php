7d0161f13c4a7966acbfc3bd59009e9932aa0a09
edicafe <edicafe.yazhu@gmail.com>
2021-03-17 21:02:20 +0530

    Submit Update

--- contact-form-handle.php
+++ contact-form-handle.php
@@ -14,5 +14,6 @@
     $headers = "From : $email_form\r\n";
     $headers .= "Reply To : $visitor_email\r\n";
     mail($to,$email_subject,$email_body,$headers);
+    echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
     header("Location : index.html");
 ?>
